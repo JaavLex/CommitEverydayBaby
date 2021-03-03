e4cae2e2d52e005fa6452e68cb1fcd357021bb29
Alexandre Javet <alexandre.javet@hotmail.com>
2021-03-03 09:53:13 +0100

    Update NeverGonnaNotCommit.js

--- NeverGonnaNotCommit.js
+++ NeverGonnaNotCommit.js
@@ -1,11 +1,20 @@
 const { promises: fs, read } = require('fs');
 const readme = require('./readme');
 
+function makeRandString(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+   }
+   return result;
+}
+
 function generateRickRoll() {
   const readmeRow = readme.split('\n');
   
   const Index = findIndex(readmeRow);
-  readmeRow[Index] = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+  readmeRow[Index] = `https://www.youtube.com/watch?v=dQw4w9WgXcQ&{makeRandString(6)}`;
 
   return readmeRow.join('\n');
 }
